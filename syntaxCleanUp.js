f3786b9cf8036b0f5bd6e00d409e3962754ac585
Jacob MacFarlane <jmacfarlane22@turing.edu>
2023-01-12 14:09:55 -0700

    Shows my final edits

--- syntaxCleanUp.js
+++ syntaxCleanUp.js
@@ -1,22 +1,30 @@
-var foods = {apples: 23, 
+var foods = {
+  apples: 23, 
   grapes: 507, 
-  eggs: 48}
-
-var person = { height: "6 ft", weight: "160 lbs" }
-
-var post = { imageSrc: "./images/beach.png",
-
-
-   caption: "At the beach with my besties", timestamp: "4:37 PM August 13, 2019",
-        number_likes: 0,
-comments: [] }
-
-
-var post2 =
-{
-      imageSrc: "./images/holiday-party.png",
-      caption: "What a great holiday party omg",
-      timestamp: "11:37 PM December 31, 2019",
-      numberLikes: 13,
-      comments: []
+  eggs: 48
 }
+
+var person = { 
+  height: "6 ft", 
+  weight: "160 lbs" 
+}
+
+var post = { 
+  imageSrc: "./images/beach.png",
+  caption: "At the beach with my besties", 
+  timestamp: "4:37 PM August 13, 2019",
+  number_likes: 0,
+  comments: [] 
+}
+
+
+var post2 = {
+  imageSrc: "./images/holiday-party.png",
+  caption: "What a great holiday party omg",
+  timestamp: "11:37 PM December 31, 2019",
+  numberLikes: 13,
+  comments: []
+}
+// For all of these objects we want to make sure the curly bracket is a space after the intial equal sign 
+// and the last curly bracket is the line after the final key. We want to make sure the indents are all the same 
+// because this is the easiest to read. We also want to make sure a comma is at the end of each value line
